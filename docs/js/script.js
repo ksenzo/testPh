fc8d9e19355f8c3f53c6e08a49723613c5650fef
ksenzo <88851206+ksenzo@users.noreply.github.com>
2022-07-21 22:43:30 +0300

    Add files via upload

---
+++ docs/js/script.js
@@ -0,0 +1,42 @@
+window.onscroll = function showHeader() {
+   let fixedHeader = document.querySelector('.header');
+   if (window.pageYOffset > 0) {
+      fixedHeader.classList.add('__fixed');
+   } else {
+      fixedHeader.classList.remove('__fixed');
+   }
+};
+
+let openMenuBtn = document.querySelector('.header__burger');
+let closeMenuBtn = document.querySelector('.header__close-menu');
+
+if (openMenuBtn) {
+   const menuActive = document.querySelector('.header__menu');
+   openMenuBtn.addEventListener('click', function () {
+      menuActive.classList.add('__active');
+      document.querySelector('body').classList.add('__noscroll');
+   });
+}
+
+const menuActive = document.querySelector('.header__menu');
+
+if (closeMenuBtn) {
+   closeMenuBtn.addEventListener('click', function () {
+      menuActive.classList.remove('__active');
+      document.querySelector('body').classList.remove('__noscroll');
+   });
+}
+
+menuActive.addEventListener('click', function(e) {
+   if (!e.target.closest('.header__menu-body')) {
+      menuActive.classList.remove('__active');
+      document.querySelector('body').classList.remove('__noscroll');
+   }
+});
+
+document.addEventListener("keydown", (e) => {
+   if(e.key === "Escape"){
+		menuActive.classList.remove('__active');
+      document.querySelector('body').classList.remove('__noscroll');
+	}
+});
